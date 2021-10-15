508dfddc4f7116372ca84b5f7923b6d710ee5334
Demarcily <liam.murphy@elev.ga.ntig.se>
2021-10-15 11:06:10 +0200

    ✨ Images

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,9 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/css/');
   eleventyConfig.addWatchTarget('./src/css/');
 
+  eleventyConfig.addPassthroughCopy('./src/images/');
+  eleventyConfig.addWatchTarget('./src/images/');
+
   return {
     dir: {
       input: "src",
